73426a7847be7c9f81558881e3db9543d03e7785
rana deeb <rannoushe92@gmail.com>
2020-08-18 11:05:45 -0700

    first commit of lesson_5

---
+++ lesson_5/practice_problem1.js
@@ -0,0 +1,7 @@
+// Advanced javascript collections
+// Practice problem 1
+let arr = ["10", "11", "9", "7", "8"];
+
+let sortedArray = arr.sort((a, b) => Number(b) - Number(a));
+
+console.log(sortedArray);
---
+++ lesson_5/practice_problem2.js
@@ -0,0 +1,25 @@
+// Advanced javascript collections
+// Practice problem 2
+// sort array of objects based on year of published, from earliest to the latest.
+
+let books = [
+  {
+    title: "One Hundred Years of Solitude",
+    author: "Gabriel Garcia Marquez",
+    published: "1967",
+  },
+  {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    published: "1925",
+  },
+  { title: "War and Peace", author: "Leo Tolstoy", published: "1869" },
+  { title: "Ulysses", author: "James Joyce", published: "1922" },
+  { title: "The Book of Kells", author: "Multiple Authors", published: "800" },
+];
+
+let sortedArray = books.sort(
+  (a, b) => Number(a.published) - Number(b.published)
+);
+
+console.log(sortedArray);
