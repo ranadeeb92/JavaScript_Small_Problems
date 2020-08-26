d2c4fc736b333ff7bcb8399cb4d9d15ea7d7b427
Rana Deeb <ranadeeb@gmail.com>
2020-08-25 18:44:56 -0700

    last update of twenty_one code

--- 06_Slightly_large_problems/test.js
+++ 06_Slightly_large_problems/test.js
@@ -13,24 +13,32 @@
 //   }
 // }
 
-
-const winningLines = [
-  [1, 2, 3], [4, 5, 6], [7, 8, 9],
-  [1, 4, 7], [2, 5, 8], [3, 6, 9],
-  [1, 5, 9], [3, 5, 7]  
-];
-
-function CheckImmediateThreat(humanSquares) {
-  return !(findThreateningLines(humanSquares).length === 0);
+function getAppropiateCardValue(card) {
+  switch (card) {
+    case 'jack' :
+    case 'queen' :
+    case 'king' :
+      return 10;
+    case 'Ace' :
+      return 11;
+    default :
+      return Number(card);
+  }
 }
 
-function findThreateningLines(humanPlayerSquares) {
-  return winningLines.filter(line => {
-    return humanPlayerSquares.includes(line[1]) &&
-      (humanPlayerSquares.includes(line[0]) || 
-      humanPlayerSquares.includes(line[2]));
-  });
+// get total cards values for player
+function getTotal(cardsArray) {
+  let cards = cardsArray.map(ele => ele[1]);
+  let total = cards.reduce((acc, val) => {
+    acc += getAppropiateCardValue(val, cards);
+    return acc;
+  }, 0);
+  if (cards.includes('Ace') && total > 21) {
+    total -= 10;
+    return total;
+  }
+  return total;
 }
 
-console.log(findThreateningLines([1, 2, 9]).filter(arr => arr.some(ele => ele === 3)));
-console.log(CheckImmediateThreat([1, 2, 9]));
+let arr = [['2', 'hearts'], ['queen', 'hearts'], ['Ace', 'hearts']];
+console.log(getTotal(arr));
--- 06_Slightly_large_problems/twenty_one/twenty_one.js
+++ 06_Slightly_large_problems/twenty_one/twenty_one.js
@@ -1,21 +1,47 @@
 
 const readline = require('readline-sync');
+const msgs = require('./messages.json');
 const CARD_VALUES = [
   'Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10','jack', 'queen', 'king'
 ];
-const SUITS = ['hearts', 'dimonds', 'clubs', 'spades'];
-
+const SUITS = ['H', 'D', 'C', 'S'];
 const NUMBER_OF_PLAYERS = 2;
-const GAME_CHOICES = ['hit', 'stay'];
+const GAME_CHOICES = ['hit', 'stay', 's', 'h'];
+const PLAY_AGAIN_ANSWERS = ['yes','no', 'y', 'n'];
+const BUST_LIMIT = 21;
+const DEALER_GOAL = 17;
+const MATCH_SCORE_GOAL = 5;
 
 // prompt msgs
 function prompt(msg) {
-  console.log(msg);
+  console.log(`>> ${msg}`);
+}
+
+// convert hand array to string for display
+function handToString(arr) {
+  let str = '';
+
+  str += arr[0][1] + ' of ' + arr[0][0];
+  for (let index = 1; index < arr.length; index++) {
+    str +=  ', ' + arr[index][1] + ' of ' + arr[index][0];
+  }
+  return str;
 }
 
 // check player(user) input
-function isValidInput(input) {
-  return GAME_CHOICES.includes(input);
+function isValidInput(input, validInputs) {
+  return validInputs.includes(input);
+}
+
+// get Player(user) Input
+function getPlayerInput(msg, isValid, validInputs) {
+  prompt(msg);
+  let answer =  readline.question();
+  while (!isValid(answer, validInputs)) {
+    prompt(`Invalid Input, ${msg}`);
+    answer = readline.question();
+  }
+  return answer.toLowerCase();
 }
 
 // initialize Deck with 52-card(4 suits, 13 values for each suit)
@@ -31,6 +57,7 @@ function initializeDeck() {
   }
   return deck;
 }
+
 
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
@@ -52,17 +79,18 @@ function getRandomSuit(deck) {
 
 //deal card to player or dealer
 function dealCard(deck, playerCrads) {
-  // get random suit with it's array of cards values
   let [suit ,suitArray] = getRandomSuit(deck);
   // get random index from suit array
   let randomCardIndex = getRandomIndex(suitArray);
   let card = suitArray[randomCardIndex];
-  // add the card to the player cards array
+  // add the card to the player cards(player or dealer) array
   playerCrads.push([suit, card]);
   // remove this card from suit array in deck obj
   deck[suit].splice(randomCardIndex,1);
 }
 
+// function to get a nested array
+// each inner array represents the playerCardsArray
 function setupPlayersCardsArrays(deck) {
   let playerNumber = 0;
   let cardsArrays = [];
@@ -77,24 +105,10 @@ function setupPlayersCardsArrays(deck) {
   return cardsArrays;
 }
 
-// get player decision (hit or stay)
-function hitOrStay(isValid) {
-  prompt('Please select hit or stay?');
-  let answer =  readline.question();
-  while (!isValid(answer)) {
-    prompt('Invalid Input, please select hit or stay');
-    answer = readline.question();
-  }
-  return answer.toLowerCase();
-}
-
-// get known dealer card
-function getKnownDealerCard(dealerCardsArray) {
-  return dealerCardsArray[0];
-}
-
+// function return true if player or dealer busts
+// else it retuens false
 function bust(total) {
-  return total > 21;
+  return total > BUST_LIMIT;
 }
 
 // get apprpraite values for (queen, jack, king, ace) cards
@@ -119,96 +133,152 @@ function getTotal(cardsArray) {
     return acc;
   }, 0);
   if (cards.includes('Ace') && total > 21) {
-    total -= 10;
-    return total;
+    cards.filter(card => card === 'Ace').forEach(_ => {
+      if (total > 21) total -= 10;
+    });
   }
   return total;
 }
 
-// player Turn returns next when player choose stay
+// player Turn returns stay when player choose stay
 // return bust if player lose
 //  or still hit
-function playerTurn(deck, isValid, playerCardsArray) {
-  let playerCardValuesTotal = getTotal(playerCardsArray);
-  prompt(`CardValuesTotal is ${playerCardValuesTotal}`);
-  while (!bust(playerCardValuesTotal)) {
-    let playerChoice = hitOrStay(isValid);
-    if (playerChoice === 'hit') {
+function playerTurn(deck, playerCardsArray, cardTotals) {
+  prompt(`Total is ${cardTotals.playerTotals}`);
+  while (!bust(cardTotals.playerTotals)) {
+    let playerChoice = getPlayerInput(
+      msgs.hitOrStay, isValidInput, GAME_CHOICES);
+    if (playerChoice[0] === 'h') {
       dealCard(deck, playerCardsArray);
-      prompt(`${playerCardsArray}`);
+      prompt(`Player hand: ${handToString(playerCardsArray)}`);
     } else {
       // if choose stay
       return 'stay';
     }
-    playerCardValuesTotal = getTotal(playerCardsArray);
-    prompt(`CardValuesTotal is ${playerCardValuesTotal}`);
+    cardTotals.playerTotals = getTotal(playerCardsArray);
+    prompt(`Total is ${cardTotals.playerTotals}`);
   }
   return 'bust';
 }
 
 // dealer turn
-function dealerTurn(deck, dealerCardsArray) {
-  let dealerCardValuesTotal = getTotal(dealerCardsArray);
-  while (dealerCardValuesTotal < 17) {
+function dealerTurn(deck, dealerCardsArray, cardTotals) {
+  while (cardTotals.dealerTotals < DEALER_GOAL) {
     dealCard(deck, dealerCardsArray);
-    dealerCardValuesTotal = getTotal(dealerCardsArray);
+    cardTotals.dealerTotals = getTotal(dealerCardsArray);
   }
-  return bust(dealerCardValuesTotal) ? 'bust' : 'stay';
+  return bust(cardTotals.dealerTotals) ? 'bust' : 'stay';
 }
 
-// compare totals if both players stay
-function compareTotals(playerCardsArray, dealerCardsArray) {
-  let playerTotal = getTotal(playerCardsArray);
-  let dealerTotal = getTotal(dealerCardsArray);
-  prompt(`Player Cards Total is ${playerTotal} and Dealer Cards Tolal is ${dealerTotal}`);
-  if (playerTotal === dealerTotal) return 'tie';
-  return playerTotal > dealerTotal ? 'Player' : 'Dealer';
-}
 
-function playGame() {
-  let winner;
-  let deckObject = initializeDeck();
-  let [playerArr, dealerArr] = setupPlayersCardsArrays(deckObject);
-  prompt(`player Cards Array ${playerArr}`);
-  prompt(`known dealer card ${getKnownDealerCard(dealerArr)}`);
-
-  let playerBustOrStay = playerTurn(deckObject, isValidInput, playerArr);
-  if (playerBustOrStay === 'bust') {
-    winner = 'Dealer';
+// compare totals
+function compareValues(handValues1, handValues2) {
+  if (handValues1 >  handValues2) {
+    return 1;
+  } else if (handValues1 < handValues2) {
+    return -1;
   } else {
-    let dealerBustOrStay = dealerTurn(deckObject, dealerArr);
-    if (dealerBustOrStay === 'bust') {
-      winner = 'Player';
+    return 0;
+  }
+}
+
+// play again
+function playAgain() {
+  let answer = getPlayerInput(msgs.playAgain, isValidInput, PLAY_AGAIN_ANSWERS);
+  return answer[0] === 'y';
+}
+
+// game logic
+function playGame(deckObject, playerArr, dealerArr) {
+  let gameResult;
+  let totals = { playerTotals : getTotal(playerArr),
+    dealerTotals : getTotal(dealerArr)};
+  if (playerTurn(deckObject, playerArr, totals) === 'bust') {
+    gameResult = 'PlayerBust';
+  } else if (dealerTurn(deckObject, dealerArr, totals) === 'bust') {
+    gameResult = 'DealerBust';
+  } else {
+    let result = compareValues(totals.playerTotals, totals.dealerTotals);
+    if (result === 0) {
+      gameResult = 'tie';
     } else {
-      winner = compareTotals(playerArr, dealerArr);
+      gameResult = result === 1 ? 'Player' : 'Dealer';
     }
   }
-  return winner;
+  return gameResult;
 }
 
-// display result
-function displayResult(result) {
+// display each game result
+function displayResult(result, playerCards, dealerCards) {
   switch (result) {
     case 'Player' :
-      prompt('Player won!');
+      prompt(msgs.playerWon);
+      break;
+    case 'PlayerBust' :
+      prompt(msgs.playerBust);
+      break;
+    case 'DealerBust' :
+      prompt(msgs.dealerBust);
       break;
     case 'Dealer' :
-      prompt('Dealer Won!');
+      prompt(msgs.dealerWon);
       break;
     default :
-      prompt('It is a tie!');
+      prompt(msgs.tie);
+  }
+  prompt(`Player Cards Total is ${getTotal(playerCards)} and Dealer Cards Tolal is ${getTotal(dealerCards)}`);
+}
+
+// function to update winner score
+function updateScores(result, scoresObj) {
+  switch (result) {
+    case 'Player' :
+    case 'DealerBust' :
+      scoresObj['playerScore'] += 1;
+      return scoresObj;
+    case 'PlayerBust' :
+    case 'Dealer' :
+      scoresObj['dealerScore'] += 1;
+      return scoresObj;
+    default :
+      return scoresObj;
   }
 }
 
-let gameResult = playGame();
-displayResult(gameResult);
-// console.log('--1--');
-// dealCard(deckObject, playerArr);
-// console.log(playerArr);
-// console.log('----');
-// console.log(deckObject);
-// console.log('---2--');
-// dealCard(deckObject, playerArr);
-// console.log(playerArr);
-// console.log('----');
-// console.log(deckObject);
+// function to check if any of the players reach to score = 5
+function checkScores(scoresObj) {
+  return Object.values(scoresObj).some(score => score === MATCH_SCORE_GOAL);
+}
+
+// display final match result
+function displayMatchWinner(scoresObj) {
+  let winner = scoresObj.playerScore === 5 ? 'Player' : 'Dealer';
+  prompt(`${winner} wins the Match!`);
+}
+
+// function to clear console screen
+function clearScreen() {
+  console.clear();
+}
+
+// game setup
+while (true) {
+  let scores = {playerScore : 0, dealerScore : 0};
+  while (!checkScores(scores)) {
+    clearScreen();
+    prompt(`Player score ${scores.playerScore} and Dealer Score ${scores.dealerScore}`);
+    let deckObject = initializeDeck();
+    let [playerArr, dealerArr] = setupPlayersCardsArrays(deckObject);
+    prompt(`player hand: ${handToString(playerArr)}`);
+    prompt(`Dealer card: ${handToString([dealerArr[0]])}`);
+    let gameResult = playGame(deckObject, playerArr, dealerArr);
+    updateScores(gameResult, scores);
+    displayResult(gameResult, playerArr, dealerArr, scores);
+    readline.question(msgs.contiueTheMatch);
+  }
+  clearScreen();
+  prompt(`Player score ${scores.playerScore} and Dealer Score ${scores.dealerScore}`);
+  displayMatchWinner(scores);
+
+  if (!playAgain()) break;
+}
